36fb1fb5ae783f1212b452eb2ebccc431e687863
Reza Paidar <reza.paidar@outlook.com>
2026-06-16 11:23:25 +0330

    chore-- create Weight Object value

--- CD.Domain.Tests/UnitTest1.cs
+++ CD.Domain.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
-﻿namespace CD.Domain.Tests
+﻿using System.Xml.Linq;
+
+namespace CD.Domain.Tests
 {
     public class UnitTest1
     {
@@ -6,8 +8,8 @@
         public void Pet_should_be_equal()
         {
             var id = Guid.NewGuid();
-            var pet1 = new Pet(id) { Name = "Belfi", Age = 7 };
-            var pet2 = new Pet(id) { Name = "Taffi", Age = 2 };
+            var pet1 = new Pet(id, "Belfi", 7, "Two-Colors",new Weight(25.2m), SexOfPet.Male) ;
+            var pet2 = new Pet(id, "Taffi", 2, "Three-Colors", new Weight(3.1m), SexOfPet.Female);
 
             Assert.True(pet1.Equals(pet2));
         }
@@ -16,8 +18,8 @@
         public void Pet_should_be_equal_using_operators()
         {
             var id = Guid.NewGuid();
-            var pet1 = new Pet(id);
-            var pet2 = new Pet(id);
+            var pet1 = new Pet(id, "Belfi", 7, "Two-Colors", new Weight(25.2m), SexOfPet.Male);
+            var pet2 = new Pet(id, "Taffi", 2, "Three-Colors", new Weight(3.1m), SexOfPet.Female);
 
             Assert.True(pet1 == pet2);
         }
@@ -27,10 +29,20 @@
         {
             var id1 = Guid.NewGuid();
             var id2 = Guid.NewGuid();
-            var pet1 = new Pet(id1);
-            var pet2 = new Pet(id2);
+            var pet1 = new Pet(id1, "Belfi", 7, "Two-Colors", new Weight(25.2m), SexOfPet.Male);
+            var pet2 = new Pet(id2, "Taffi", 2, "Three-Colors", new Weight(3.1m), SexOfPet.Female);
 
             Assert.True(pet1 != pet2);
         }
+
+        [Fact]
+        public void Weight_should_be_equal()
+        {
+            var w1 = new Weight(25.2m);
+            var w2 = new Weight(25.2m);
+
+            Assert.True(w1 == w2);
+        }
+
     }
 }
--- CD.Domain/Pet.cs
+++ CD.Domain/Pet.cs
@@ -4,12 +4,25 @@
     {
         public string Name { get; init; }
         public int Age { get; init; }
-        public string Color { get; set; }
-        public decimal Weight { get; set; }
+        public string Color { get; init; }
+        public Weight Weight { get; init; }
+        public SexOfPet SexOfPet { get; init; }
 
-        public Pet(Guid id)
+
+        public Pet(Guid id, string name, int age, string color, Weight weight, SexOfPet sexOfPet)
         {
             Id = id;
+            Name = name;
+            Age = age;
+            Color = color;
+            Weight = weight;
+            SexOfPet = sexOfPet;
         }
+    }
+
+    public enum SexOfPet
+    {
+        Male,
+        Female,
     }
 }
