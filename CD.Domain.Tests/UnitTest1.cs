1363e7352f97b56cd65802f7fd32d3c042a6b0f4
Reza Paidar <reza.paidar@outlook.com>
2026-06-16 11:34:05 +0330

    chor-- Create Breed class

--- CD.Domain.Tests/UnitTest1.cs
+++ CD.Domain.Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace CD.Domain.Tests
         public void Pet_should_be_equal()
         {
             var id = Guid.NewGuid();
-            var pet1 = new Pet(id, "Belfi", 7, "Two-Colors",new Weight(25.2m), SexOfPet.Male) ;
+            var pet1 = new Pet(id, "Belfi", 7, "Two-Colors", new Weight(25.2m), SexOfPet.Male);
             var pet2 = new Pet(id, "Taffi", 2, "Three-Colors", new Weight(3.1m), SexOfPet.Female);
 
             Assert.True(pet1.Equals(pet2));
@@ -44,5 +44,13 @@ namespace CD.Domain.Tests
             Assert.True(w1 == w2);
         }
 
+        [Fact]
+        public void Weight_Range_should_be_equal()
+        {
+            var w1 = new WeightRange(2m, 25.2m);
+            var w2 = new WeightRange(2m, 25.2m);
+
+            Assert.True(w1 == w2);
+        }
     }
 }
---
+++ CD.Domain/Breed.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD.Domain
+{
+    public class Breed: Entity
+    {
+        public string Name { get; set; }
+
+        public WeightRange MaleIdealWeight { get; set; }
+        public WeightRange FemaleIdealWeight { get; set; }
+
+        /*Instead of these properties*/
+        //public decimal FromMaleIdealWeight { get; set; }
+        //public decimal ToMaleIdealWeight { get; set; }
+        //public decimal FromFemaleIdealWeight { get; set; }
+        //public decimal ToFemaleIdealWeight { get; set; }
+
+        public Breed(string name, WeightRange maleIdealWeight, WeightRange femaleIdealWeight)
+        {
+            Name = name;
+            MaleIdealWeight = maleIdealWeight;
+            FemaleIdealWeight = femaleIdealWeight;
+        }
+    }
+
+    public record WeightRange
+    {
+        public decimal From { get; init; }
+        public decimal To { get; init; }
+
+        public WeightRange(decimal from, decimal to)
+        {
+            From = from;
+            To = to;
+        }
+    }
+}
