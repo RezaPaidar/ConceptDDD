89186684ec2f6dbe40d87a3dce6abc0a79dc2706
Reza Paidar <reza.paidar@outlook.com>
2026-06-18 10:37:03 +0330

    chore-- complete the logics for consultation

--- CD.Domain.Clinic/Consultaion.cs
+++ CD.Domain.Clinic/Consultaion.cs
@@ -5,19 +5,55 @@ namespace CD.Domain.Management.Clinic
 {
     public class Consultaion : AggregateRoot
     {
+        public DateTime StartedAdd { get; init; }
+        public DateTime? EndedAt { get; private set; }
         public Text Diagnosis { get; private set; }
         public Text Treatment { get; private set; }
         public PatientId PatientId { get; set; }
         public Weight CurrentWeight { get; private set; }
         public ConsultationStatus Status { get; private set; }
 
+        public Consultaion(PatientId patientId)
+        {
+            Id = Guid.NewGuid();
+            this.PatientId = patientId;
+            Status = ConsultationStatus.Open;
+            this.StartedAdd = DateTime.UtcNow;
+        }
+
+        public void End()
+        {
+            ValidationConsultationStatus();
+            if (this.Diagnosis == null || this.Treatment == null || this.CurrentWeight == null)
+            {
+                throw new InvalidOperationException("Cannot close consultation without diagnosis, treatment, and weight.");
+            }
+            this.EndedAt = DateTime.UtcNow;
+        }
 
         public void SetWeight(Weight weight)
         {
-            CurrentWeight = weight;
+            ValidationConsultationStatus();
+            this.CurrentWeight = weight;
         }
 
+        private void ValidationConsultationStatus()
+        {
+            if (Status == ConsultationStatus.Close)
+                throw new InvalidOperationException("Cannot set weight for a closed consultation.");
+        }
 
+        public void SetDiagnosis(Text diagnosis)
+        {
+            ValidationConsultationStatus();
+            this.Diagnosis = diagnosis;
+        }
+
+        public void SetTreatment(Text treatment)
+        {
+            ValidationConsultationStatus();
+            this.Treatment = treatment;
+        }
     }
 
     public enum ConsultationStatus
